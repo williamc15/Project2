07f5a20e5048d80eb5793179c81483b72ea3dd6d
Alex Thornton <thornton@ics.uci.edu>
2024-04-22 13:15:20 -0700

    Spring 2024 starting point

---
+++ p2app/__init__.py
@@ -0,0 +1,12 @@
+# p2app/__init__.py
+#
+# ICS 33 Spring 2024
+# Project 2: Learning to Fly
+#
+# Initialization module for the p2app package.
+#
+# YOU WILL NOT NEED TO MODIFY THIS FILE AT ALL
+
+from .engine import Engine
+from .events import EventBus
+from .views import MainView
---
+++ p2app/engine/__init__.py
@@ -0,0 +1,10 @@
+# p2app/engine/__init__.py
+#
+# ICS 33 Spring 2024
+# Project 2: Learning to Fly
+#
+# Initialization module for the p2app.engine package.
+#
+# YOU WILL NOT NEED TO MODIFY THIS FILE AT ALL
+
+from .main import Engine
